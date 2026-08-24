b0d3e4f6c1417daab75ca1fd0c345c333e031c18
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-08-24 18:57:01 -0300

    add: dark theme and start game logic

--- src/main.js
+++ src/main.js
@@ -1,3 +1,30 @@
 import './style.css';
 
+let darkThemeButton = document.getElementById("dark-theme");
 const cells = document.querySelectorAll("#game-board button");
+
+const playerX = {
+    name: "PLAYER X",
+    score: 0,
+    symbol: "X"
+}
+
+const playerO = {
+    name: "PLAYER O",
+    score: 0,
+    symbol: "O"
+}
+
+let ties = 0;
+let currentPlayer = playerX;
+
+darkThemeButton.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+});
+
+cells.forEach((cells) => {
+    cells.addEventListener("click", () => {
+        
+    });
+});
+
